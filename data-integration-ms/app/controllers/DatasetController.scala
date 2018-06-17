d0bdee2376b5971bc29a6295122c53466de0088b
Pablo Osinaga <paguos@gmail.com>
2018-06-17 19:46:34 +0200

    Add API routes and ability to upload data

---
+++ data-integration-ms/app/controllers/DatasetController.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import java.nio.file.Paths
+
+import javax.inject._
+import play.Play
+import play.api.Logger
+import play.api.mvc._
+
+@Singleton
+class DatasetController  @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+
+  def index = Action {Ok(views.html.todo())}
+
+  def read(id: String) = Action {Ok(views.html.todo())}
+
+  def update(id: String) = Action {Ok(views.html.todo())}
+
+  def upload = Action(parse.multipartFormData) { request =>
+    request.body.file("dataset").map { dataset =>
+
+      // only get the last part of the filename
+      // otherwise someone can send a path like ../../home/foo/bar.txt to write to other files on the system
+      val filename = Paths.get(dataset.filename).getFileName
+      var appPath = Play.application().path()
+      Logger.info(s"$appPath")
+
+      dataset.ref.moveTo(Paths.get(s"/$appPath/datasets/$filename"), replace = true)
+      Logger.info(s"File added!")
+      Ok("File uploaded")
+    }.getOrElse {
+      Logger.info(s"File not added!")
+      Redirect(routes.HomeController.index).flashing(
+        "error" -> "Missing file")
+    }
+  }
+
+
+}
---
+++ data-integration-ms/app/controllers/IntegrationController.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import java.nio.file.Paths
+
+import javax.inject._
+import play.Play
+import play.api.Logger
+import play.api.mvc._
+
+@Singleton
+class IntegrationController  @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+
+  def index = Action {Ok(views.html.todo())}
+
+  def read(id: String) = Action {Ok(views.html.todo())}
+
+  def update(id: String) = Action {Ok(views.html.todo())}
+
+  def upload = Action {Ok(views.html.todo())}
+
+
+}
