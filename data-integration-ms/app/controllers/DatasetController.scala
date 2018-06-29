c74a7200af999a3f686924bac9ce157dd8f2f6a3
Pablo Osinaga <paguos@gmail.com>
2018-06-29 22:39:40 +0200

    Implementation of basic funtionality for DatabaseController

--- data-integration-ms/app/controllers/DatasetController.scala
+++ data-integration-ms/app/controllers/DatasetController.scala
@@ -3,28 +3,23 @@ package controllers
 import java.io.File
 import java.nio.file.Paths
 
-import akka.http.scaladsl.model.HttpHeader.ParsingResult
 import javax.inject._
-import play.Play
 import play.api.Logger
-import play.api.data.Form
 import play.api.libs.json.Json
-import play.api.libs.json.JsonNaming.Identity
 import play.api.mvc._
-import services.Counter
 import play.api.libs.json._
 
-import scala.concurrent._
-import scala.concurrent.duration._
-import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.{Await, Future, Promise}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 
 @Singleton
 class DatasetController  @Inject()(cc: ControllerComponents) extends AbstractController(cc){
 
+  /* Classes and implicit values to generate the Dataset json: */
+
   case class Dataset(id: Int, name: String)
-  case class Summary(datasets: Seq[Dataset])
+  case class DatasetCollection(datasets: Seq[Dataset])
 
   implicit val datasetWrites = new Writes[Dataset] {
     def writes(dataset: Dataset) = Json.obj(
@@ -33,131 +28,146 @@ class DatasetController  @Inject()(cc: ControllerComponents) extends AbstractCon
     )
   }
 
-  implicit val summaryWrites = new Writes[Summary] {
-    def writes(summary: Summary) = Json.obj(
+  implicit val summaryWrites = new Writes[DatasetCollection] {
+    def writes(summary: DatasetCollection) = Json.obj(
       "datasets" -> summary.datasets
     )
   }
 
-
+  /*
+  Gets the list  of files with a list of extensions at a directory
+   */
   def getListOfFiles(dir: File, extensions: List[String]): List[File] = {
     dir.listFiles.filter(_.isFile).toList.filter { file =>
       extensions.exists(file.getName.endsWith(_))
     }
   }
 
+  /*
+  Get a files with an specific id
+   */
   def getListFile(dir: File, id: Int): List[File] = {
     dir.listFiles.filter(_.isFile).toList.filter { file =>
       val fileName =file.getName()
       val args = fileName.split("-")
       val dsId = args.apply(0)
-
-      Logger.info("Infox: " + dsId)
-
       id == dsId.toInt
     }
   }
 
+  /*
+  Returns a json files with the information of all existing dataset files
+  Responsible for the GET /api/datasets route
+   */
   def index = Action {
-
-    val appPath = Play.application().path()
+    Logger.info("Calling index action ...")
+    val currentDirectory = new java.io.File(".").getCanonicalPath
     val okFileExtensions = List("csv")
-    val dir = new File(s"/$appPath/datasets/")
-
-    val x = Future {
-      dir.listFiles.filter(_.isFile).toList
-    }
+    val files = getListOfFiles(new File(s"/$currentDirectory/datasets/"), okFileExtensions)
 
     var dsSeq = Seq[Dataset]()
 
-    val r = x map { list =>
-
-      for (file <- list){
-        val fileName = file.getName
-        val datasetArgs = fileName.split("-")
-
-        val id = datasetArgs.apply(0)
-        val name = datasetArgs.apply(1)
-        dsSeq = dsSeq :+ Dataset(id.toInt, name)
-
-        Logger.info(list.toString())
-
-        val values = Summary(dsSeq)
-        val json = Json.toJson(values)
-
-        Ok(Json.prettyPrint(json))
-
-      }
-    }
-
-    Await.ready(r, 1000000 nanos)
-
-    val values = Summary(dsSeq)
-    val json = Json.toJson(values)
-
-    Ok(Json.prettyPrint(json))
-  }
-
-  def read(id: String) = Action {
-
-    val appPath = Play.application().path()
-    val dir = new File(s"/$appPath/datasets/")
-
-    val files = getListFile(dir, id.toInt)
-    var dsSeq = Seq[Dataset]()
-
-
-    for(file <- files) {
-
+    for (file <- files){
       val fileName = file.getName
-      val datasetArgs = fileName.split("-")
+      Logger.info("Found file: " + fileName)
 
+      val datasetArgs = fileName.split("-")
       val id = datasetArgs.apply(0)
       val name = datasetArgs.apply(1)
       dsSeq = dsSeq :+ Dataset(id.toInt, name)
-
     }
 
-    if (dsSeq.length < 1) NotFound(views.html.todo())
-    else Ok(Json.prettyPrint(Json.toJson(dsSeq.head)))
+    val values = DatasetCollection(dsSeq)
+    val json = Json.toJson(values)
+    Logger.info("End index action")
+    Ok(Json.prettyPrint(json))
+  }
+
+  /*
+  Gets an specific dataset file by id
+  Responsible for the GET /api/dataset/:id route
+   */
+  def read(id: String) = Action {
+
+    Logger.info(s"Calling read action (id:$id) ...")
+
+    val currentDirectory = new java.io.File(".").getCanonicalPath
+    val dir = new File(s"/$currentDirectory/datasets/")
+
+    val files = getListFile(dir, id.toInt)
+    Logger.info(s"Founded files for id=$id\n -> $files")
+
+    if (files.length < 1) NotFound(views.html.todo())
+    else Ok.sendFile(new java.io.File(dir + "/" + files.head.getName))
 
   }
 
   def update(id: String) = Action {Ok(views.html.todo())}
 
+  /*
+  Uploads a new dataset file
+  Responsible for the POST /api/dataset route
+   */
   def upload = Action(parse.multipartFormData) { request =>
 
-    var appPath = Play.application().path()
+    Logger.info("Calling upload action ...")
+
+    val currentDirectory = new java.io.File(".").getCanonicalPath
     val okFileExtensions = List("csv")
-    val files = getListOfFiles(new File(s"/$appPath/datasets/"), okFileExtensions)
+    val files = getListOfFiles(new File(s"/$currentDirectory/datasets/"), okFileExtensions)
     var name = ""
-    var id = "%03d".format(files.length + 1)
+    val id = "%03d".format(files.length + 1)
 
     request.body.asFormUrlEncoded("datasetName").map( { datasetName =>
         name = datasetName.toString.trim
     })
 
+    Logger.info("New dataset name: " + name)
 
     request.body.file("dataset").map { dataset =>
-      // only get the last part of the filename
-      // otherwise someone can send a path like ../../home/foo/bar.txt to write to other files on the system
-      val filename = Paths.get(dataset.filename).getFileName
-      var appPath = Play.application().path()
 
-      dataset.ref.moveTo(Paths.get(s"/$appPath/datasets/$id-$name.csv"), replace = true)
+      val fileName = dataset.filename
+      Logger.info(s"Uploading file: $fileName")
+
+      dataset.ref.moveTo(Paths.get(s"/$currentDirectory/datasets/$id-$name.csv"), replace = true)
       Logger.info(s"File $id-$name.csv added!")
+
       Ok(views.html.index())
     }.getOrElse {
       Logger.error(s"File not found im form")
       Redirect(routes.HomeController.index).flashing(
         "error" -> "Missing file")
     }
-
-
   }
 
-  def delete(id: String) = Action {Ok(views.html.todo())}
+  /*
+  Deletes a dataset file with a given id
+  Responsible for the DELETE /api/dataset/:id route
+   */
+  def delete(id: String) = Action {
 
+    //TODO: implement async deletion with Monads and Futures
+    /*implicit class FileMonads(f: File) {
+      def check = Future{ f.exists } //returns "Future" monad
+      def remove = Future{ f.delete() } //returns "Future" monad
+    }*/
 
+    Logger.info(s"Calling delete action (id:$id) ...")
+
+    val currentDirectory = new java.io.File(".").getCanonicalPath
+    val dir = new File(s"/$currentDirectory/datasets/")
+
+    val files = getListFile(dir, id.toInt)
+    var deleted = false
+
+    for (file <- files){
+      val fileName = file.getName
+      Logger.info(s"Found file with id=$id\n -> $fileName")
+      if (file.delete()) deleted = true
+    }
+
+    if (deleted) Ok(views.html.todo())
+    else NotFound(views.html.todo())
+  }
 
 }
