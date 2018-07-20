711825c03a699da36674c628ca0db5bbed64dec0
Venkat <subbaraocse9@gmail.com>
2018-07-20 22:40:29 +0200

    Move functions to reusable classes

---
+++ backend/src/main/scala/utilities/Utilities.scala
@@ -0,0 +1,27 @@
+package utilities
+
+import inputreader.CSVReader
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+class Utilities extends Serializable {
+
+  def rowConvert(x:Row,len:Int):Array[String] = {
+    var y:Array[String] = new Array[String](len)
+    for (i <- 0 to len-1){
+      y(i)=String.valueOf(x.get(i))
+    }
+    y
+  }
+
+  def readInputAsDataFrame(file1:String,file2:String,spark:SparkSession):Array[DataFrame]={
+    val csvReader = new CSVReader(spark);
+    val dfA = csvReader.readData(file1, "\t")
+    val dfB = csvReader.readData(file2, "\t")
+    Array(dfA,dfB)
+  }
+
+}
+
+object Utilities {
+
+}
